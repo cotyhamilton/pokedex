a96b7708ce12276b4493d06c3a6ef24fc30870d7
cotyhamilton <cotyhamilton@gmail.com>
2020-09-26 21:21:47 +0000

    Deploying to gh-pages from  @ fc33120f5de0ff76634b3d57491dbb56bab73eb3 🚀

---
+++ precache-manifest.f8d56febd1b8a4cf31eaaa9f030fba11.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d5855b712ed3df56f06e4d67668b39c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ec9873f91e6c3b269e65",
+    "url": "/static/css/main.a13190e8.chunk.css"
+  },
+  {
+    "revision": "47e7d1ea47dfbc9a121a",
+    "url": "/static/js/2.ca6e5cd2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ca6e5cd2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec9873f91e6c3b269e65",
+    "url": "/static/js/main.808ce9f7.chunk.js"
+  },
+  {
+    "revision": "250103716dfc89e5abb8",
+    "url": "/static/js/runtime-main.9ff9079d.js"
+  }
+]);
