6934dbaceaf51104a8dc64141d6fcc44ac45b4cd
cotyhamilton <cotyhamilton@gmail.com>
2020-09-26 21:32:40 +0000

    Deploying to gh-pages from  @ 400d7e92f470feb4b565a1046096c286b9ad9b09 🚀

---
+++ precache-manifest.bbdb0a1c5890fc2f375b13f28afc8e89.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1e369aff08f7aa04d0d4fd090cc5292",
+    "url": "/pokedex/index.html"
+  },
+  {
+    "revision": "b50e286063641d1ec72a",
+    "url": "/pokedex/static/css/main.a13190e8.chunk.css"
+  },
+  {
+    "revision": "47e7d1ea47dfbc9a121a",
+    "url": "/pokedex/static/js/2.ca6e5cd2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pokedex/static/js/2.ca6e5cd2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b50e286063641d1ec72a",
+    "url": "/pokedex/static/js/main.7784cd4b.chunk.js"
+  },
+  {
+    "revision": "5d5b7317cf43a0b8925c",
+    "url": "/pokedex/static/js/runtime-main.2d94522c.js"
+  }
+]);
