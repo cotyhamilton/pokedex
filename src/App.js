a17f1729b6b31b78d955c6f3b1d1bbe33b7d7612
Coty <cotyhamilton@gmail.com>
2020-09-27 22:36:25 -0500

    Overhaul search feature, added tags for types and generations (#4)

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Card from './Card';
 import { Icon, Input } from 'semantic-ui-react';
 import Worker from 'workerize-loader!./workers/Worker'; // eslint-disable-line import/no-webpack-loader-syntax
 import WorkerPool from './workers/WorkerPool';
-import { TYPES } from './lib/constants';
+import { TYPES, GENERATIONS } from './lib/constants';
 
 const Pokedex = require('pokeapi-js-wrapper');
 const P = new Pokedex.Pokedex();
@@ -19,6 +19,9 @@ function App() {
   const [allPokemon, setAllPokemon] = useState([]);
   const [search, setSearch] = useState('');
   const [pokemonByType, setPokemonByType] = useState({});
+  const [pokemonByGeneration, setPokemonByGeneration] = useState({});
+  const [activeType, setActiveType] = useState('');
+  const [activeGen, setActiveGen] = useState('');
 
   useEffect(() => {
     getPokemon();
@@ -29,6 +32,11 @@ function App() {
       pokemon.map((pokemon) => {
         getPokemonData(pokemon.name);
       })
+    }
+    else {
+      if (!search && allPokemon.length) {
+        setPokemon([...allPokemon.slice(0,offset)])
+      }
     }
   }, [pokemon]);
 
@@ -51,22 +59,78 @@ function App() {
   }, [])
 
   useEffect(() => {
+    [...Array(GENERATIONS).keys()].map((i) => {
+      const gen = i + 1;
+      updatePokemonByGen(gen);
+    });
+  }, [])
+
+  useEffect(() => {
     if (search === '') {
+      setActiveGen('');
+      setActiveType('');
       const list = allPokemon.slice(offset, offset + limit);
       setPokemon((prevState) => ([...list]));
     }
     else {
-      if (TYPES.includes(search.toLowerCase())) {
-        setPokemon([...pokemonByType[search.toLowerCase()]]);
+      if (search.includes(':')) {
+        const query = (search.split(/:| /)).filter((s, index) => (s !== '')).map(s => s.toLowerCase());
+        if (query.length > 1) {
+          if (query.includes('gen')) {
+            const gen = query[query.indexOf('gen') + 1];
+            if (gen >= 1 && gen <= GENERATIONS) {
+              setActiveGen(gen);
+            } else { setActiveGen('') }
+          } else { setActiveGen('') }
+          if (query.includes('type')) {
+            const type = query[query.indexOf('type') + 1];
+            if (TYPES.includes(type)) {
+              setActiveType(type);
+            } else { setActiveType('') }
+          } else { setActiveType('') }
+        } else { setActiveType(''); setActiveGen(''); }
       }
       else {
+        setActiveType('');
+        setActiveGen('');
         const result = allPokemon.filter((pokemon, index) => {
           return pokemon.name.includes(search.toLowerCase()) || (index + 1).toString().includes(search);
         });
+        console.log(result);
         setPokemon([...result]);
       }
     }
   }, [search]);
+
+  // refactor this, wtf are you doing
+  useEffect(() => {
+    if (!activeGen && !activeType) {
+      return;
+    }
+    let filteredPokemon = [];
+    if (activeGen) {
+      allPokemon.map((pokemon) => {
+        if (pokemonByGeneration[activeGen].includes(pokemon.name)) {
+          filteredPokemon.push(pokemon);
+        }
+      })
+    }
+    if (activeType) {
+      if (filteredPokemon.length) {
+        filteredPokemon = filteredPokemon.filter((pokemon) => {
+          return pokemonByType[activeType].includes(pokemon.name)
+        })
+      }
+      else {
+        allPokemon.map((pokemon) => {
+          if (pokemonByType[activeType].includes(pokemon.name)) {
+            filteredPokemon.push(pokemon);
+          }
+        })
+      }
+    }
+    setPokemon([...filteredPokemon]);
+  }, [activeGen, activeType])
 
   const getPokemon = async () => {
     if (allPokemon.length > offset + limit) {
@@ -109,9 +173,18 @@ function App() {
     let list = [];
     const data = await P.getTypeByName(type);
     data.pokemon.map((pokemon) => {
-      list.push(pokemon.pokemon);
+      list.push(pokemon.pokemon.name);
     })
     setPokemonByType((prevState) => ({...prevState, [type]: list}))
+  }
+
+  const updatePokemonByGen = async (gen) => {
+    const data = await P.getGenerationByName(gen);
+    let names = [];
+    data.pokemon_species.map((pokemon) => {
+      names.push(pokemon.name)
+    })
+    setPokemonByGeneration((prevState) => ({...prevState, [gen]: names}))
   }
 
   return (
@@ -121,7 +194,7 @@ function App() {
       </header>
       <div className="home">
         <h1>Pokédex</h1>
-        <p>Search for Pokémon by name or using the National Pokédex number</p>
+        <p>Search for Pokémon by name, National Pokédex number, or tags like <code>type:grass gen:5</code></p>
         <Input
           fluid
           icon="search"
